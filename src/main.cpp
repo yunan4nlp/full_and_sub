28a3d852c0837ab2561fa17a0943c8dff36e7d59
YuNan <yunan.hlju@gmail.com>
2017-10-02 14:26:19 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+bool isFullUp(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state);
+bool isFullDown(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state);
+bool isSubUp(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state);
+bool isSubDown(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state);
+int getCharactersFromUTF8String(const std::string &s, std::vector<string>& sentence);
+
+int main(int argc, char* argv[]){
+	ifstream inf(argv[1]);
+	ofstream full_word(argv[2]);
+	ofstream sub_word(argv[3]);
+	string word;
+	map<string, long> word_state;
+	while(inf>>word){
+		word_state[word]++;
+	}
+	vector<string> chars;
+	string tmp;
+	int char_num;
+	map<string, int> sub;
+	for (map<string, long>::iterator it = word_state.begin(); it != word_state.end(); it++) {
+		word = it->first;
+		if (isFullUp(it, word, word_state) && isFullDown(it, word, word_state))
+			full_word << word << endl;
+		getCharactersFromUTF8String(word, chars);
+		tmp = "";
+		char_num = chars.size();
+		for(int idx = 0; idx < char_num - 1; idx++){
+			tmp += chars[idx];
+			if (sub.find(tmp) == sub.end())
+				if (isSubUp(it, tmp, word_state) && isSubDown(it, tmp, word_state))
+					sub[tmp] = 1;
+		}
+	}
+	for(map<string,int>::iterator it = sub.begin(); it != sub.end(); it++) {
+		sub_word << it->first << endl;
+	}
+	inf.close();
+	full_word.close();
+	sub_word.close();
+	cout << "ok" << endl;
+	return 0;
+}
+
+bool isFullUp(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state){
+		bool IS_FULL = true;
+		map<string, long>::iterator up = it;
+		if (up != word_state.begin())
+			up--;
+		string curWord;
+		while(up != word_state.begin()) {
+			curWord = up->first;
+			if ((curWord)[0] != str[0])
+				break;
+			if ((curWord).length() >= str.length()) {
+				string tmp = (curWord).substr(0, str.length());
+				if (tmp == str) {
+					IS_FULL = false;
+					break;
+				}
+			}
+			up--;
+		}
+		return IS_FULL;
+}
+
+bool isFullDown(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state) {
+		bool IS_FULL = true;
+		map<string, long>::iterator down = it;
+		if (down != word_state.end())
+			down++;
+		string curWord;
+		while(down != word_state.end()) {
+			curWord = down->first;
+			if ((curWord)[0] != str[0])
+				break;
+			if ((curWord).length() >= str.length()) {
+				string tmp = (curWord).substr(0, str.length());
+				if (tmp == str) {
+					IS_FULL = false;
+					break;
+				}
+			}
+			down++;
+		}
+		return IS_FULL;
+}
+
+bool isSubUp(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state) {
+		bool IS_SUB = true;
+		map<string, long>::iterator up = it;
+		if (up != word_state.begin())
+			up--;
+		string curWord;
+		while(up != word_state.begin()) {
+			curWord = up->first;
+			if (curWord == str) {
+				IS_SUB = false;
+				break;
+			}
+			up--;
+		}
+		return IS_SUB;
+}
+
+bool isSubDown(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state) {
+		bool IS_SUB = true;
+		map<string, long>::iterator down = it;
+		if (down != word_state.end())
+			down++;
+		string curWord;
+		while(down != word_state.end()) {
+			curWord = down->first;
+			if (curWord == str) {
+				IS_SUB = false;
+				break;
+			}
+			down++;
+		}
+		return IS_SUB;
+}
+
+int getCharactersFromUTF8String(const std::string &s, std::vector<string>& sentence) {
+	sentence.clear();
+	unsigned long int idx = 0;
+	unsigned long int len = 0;
+	while (idx < s.length()) {
+		if ((s[idx] & 0x80) == 0) {
+			sentence.push_back(s.substr(idx, 1));
+			++len;
+			++idx;
+		}
+		else if ((s[idx] & 0xE0) == 0xC0) {
+			sentence.push_back(s.substr(idx, 2));
+			++len;
+			idx += 2;
+		}
+		else if ((s[idx] & 0xF0) == 0xE0) {
+			sentence.push_back(s.substr(idx, 3));
+			++len;
+			idx += 3;
+		}
+		else {
+			if (s.length() - idx < 4)
+			{
+				sentence.push_back(s.substr(idx));
+				++len;
+				idx = s.length();
+			}
+			else
+			{
+				sentence.push_back(s.substr(idx, 4));
+				++len;
+				idx += 4;
+			}
+		}
+	}
+	if (idx != s.length()) {
+		//std::cerr << "Warning: " << "in utf.h getCharactersFromUTF8String: std::string '" << s << "' not encoded in utf-8" << std::endl;
+		return len + 1;
+	}
+
+	return len;
+}
