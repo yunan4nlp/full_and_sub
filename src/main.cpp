c10d13f06c66edf5bb92e696355a2599e064b9d9
YuNan <yunan.hlju@gmail.com>
2017-10-03 13:54:06 +0800

    fast

--- src/main.cpp
+++ src/main.cpp
@@ -7,132 +7,77 @@
 
 using namespace std;
 
-bool isFullUp(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state);
 bool isFullDown(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state);
-bool isSubUp(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state);
-bool isSubDown(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state);
 int getCharactersFromUTF8String(const std::string &s, std::vector<string>& sentence);
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 	ifstream inf(argv[1]);
 	ofstream full_word(argv[2]);
 	ofstream sub_word(argv[3]);
 	string word;
 	map<string, long> word_state;
-	while(inf>>word){
+	while (inf >> word) {
 		word_state[word]++;
 	}
-	cout << "word size:" << word_state.size() << endl;
 	map<string, int> sub;
 	map<string, int> full;
 omp_set_num_threads(24);
 #pragma omp parallel 
 	for (map<string, long>::iterator it = word_state.begin(); it != word_state.end(); it++) {
-		vector<string> chars;
-		string tmp;
-		int char_num;
 		string curword;
 		curword = it->first;
-		if (isFullUp(it, curword, word_state) && isFullDown(it, curword, word_state))
+		if (isFullDown(it, curword, word_state)) {
 #pragma omp critical 
 			full[curword] = 1;
+		}
+	}
+#pragma omp parallel 
+	for (map<string, long>::iterator it = word_state.begin(); it != word_state.end(); it++) {
+		vector<string> chars;
+		string curword;
+		curword = it->first;
 		getCharactersFromUTF8String(curword, chars);
-		tmp = "";
-		char_num = chars.size();
-		for(int idx = 0; idx < char_num - 1; idx++){
+		int char_num = chars.size();
+		string tmp = "";
+		for (int idx = 0; idx < char_num - 1; idx++) {
 			tmp += chars[idx];
+			if (full.find(tmp) == full.end()) {
 #pragma omp critical 
-			if (sub.find(tmp) == sub.end())
-				if (isSubUp(it, tmp, word_state) && isSubDown(it, tmp, word_state))
-#pragma omp critical 
-					sub[tmp] = 1;
+				sub[tmp] = 1;
+			}
 		}
 	}
 
-	for(map<string,int>::iterator it = full.begin(); it != full.end(); it++) {
+	for (map<string, int>::iterator it = full.begin(); it != full.end(); it++) {
 		full_word << it->first << endl;
 	}
 
-	for(map<string,int>::iterator it = sub.begin(); it != sub.end(); it++) {
+	for (map<string, int>::iterator it = sub.begin(); it != sub.end(); it++) {
 		sub_word << it->first << endl;
 	}
 
 	inf.close();
 	full_word.close();
 	sub_word.close();
-	cout << "ok" << endl;
 	return 0;
 }
 
-bool isFullUp(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state){
-		bool IS_FULL = true;
-		map<string, long>::iterator up = it;
-		up--;
-		string sub_str;
-		if (up != word_state.end()) {
-			string curWord = up->first;
-			int str_len = str.length();
-			if(str_len <= curWord.length()) {
-				 sub_str= curWord.substr(0, str_len);
-				 if (sub_str == str)
-					 IS_FULL = false;
-			}
-		}
-		return IS_FULL;
-}
-
 bool isFullDown(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state) {
-		bool IS_FULL = true;
-		map<string, long>::iterator down = it;
-		down++;
-		string sub_str;
-		if (down != word_state.end()) {
-			string curWord = down->first;
-			int str_len = str.length();
-			if(str_len <= curWord.length()) {
-				 sub_str= curWord.substr(0, str_len);
-				 if (sub_str == str) {
-					 IS_FULL = false;
-				 }
+	bool IS_FULL = true;
+	map<string, long>::iterator down = it;
+	down++;
+	string sub_str;
+	if (down != word_state.end()) {
+		string curWord = down->first;
+		int str_len = str.length();
+		if (str_len <= curWord.length()) {
+			sub_str = curWord.substr(0, str_len);
+			if (sub_str == str) {
+				IS_FULL = false;
 			}
 		}
-		return IS_FULL;
-}
-
-bool isSubUp(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state) {
-		bool IS_SUB = true;
-		map<string, long>::iterator up = it;
-		up--;
-		string curWord;
-		while(up != word_state.end()) {
-			curWord = up->first;
-			if (curWord[0] != str[0])
-				break;
-			if (curWord == str) {
-				IS_SUB = false;
-				break;
-			}
-			up--;
-		}
-		return IS_SUB;
-}
-
-bool isSubDown(const map<string, long>::iterator& it, const string& str, const map<string, long>& word_state) {
-		bool IS_SUB = true;
-		map<string, long>::iterator down = it;
-		down++;
-		string curWord;
-		while(down != word_state.end()) {
-			curWord = down->first;
-			if (curWord[0] != str[0])
-				break;
-			if (curWord == str) {
-				IS_SUB = false;
-				break;
-			}
-			down++;
-		}
-		return IS_SUB;
+	}
+	return IS_FULL;
 }
 
 int getCharactersFromUTF8String(const std::string &s, std::vector<string>& sentence) {
